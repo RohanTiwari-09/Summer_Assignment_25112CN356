e2042d6b31945ffb4fb6796d450675b364098578
RohanTiwari-09 <rohantiwari09052007@gmail.com>
2026-06-04 21:47:25 +0530

    changes

--- DAY_5/factor_of_number.c
+++ DAY_5/factor_of_number.c
@@ -2,7 +2,7 @@
 
 int main(){
     int n;
-    printf("enter the number to be get factors");
+    printf("enter the number to be get factors:");
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
         if(n%i==0){
