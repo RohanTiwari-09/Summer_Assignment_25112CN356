acc6813e8849fb8a1cd11d55bef636291a4f7db6
RohanTiwari-09 <rohantiwari09052007@gmail.com>
2026-06-09 23:00:28 +0530

    change in ninth commit

--- DAY_9/reverse_star_pattern.c
+++ DAY_9/reverse_star_pattern.c
@@ -2,7 +2,7 @@
 
 int main(){
     int n;
-    printf("enter the number of rows in pyramid:");
+    printf("enter the number of rows in reverse star pattern:");
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
         for(int j=n;j>=i;j--){
